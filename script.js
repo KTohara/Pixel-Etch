44ae501c304aef57082b4b655f8bfca3084659b3
kenta <ktohara@gmail.com>
2022-02-25 14:26:27 -0500

    Add random, erase functions

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = '#333333';
+const DEFAULT_MODE = 'color'
 
 let currentColor = DEFAULT_COLOR;
 let currentSize = DEFAULT_SIZE;
-
+let currentMode = DEFAULT_MODE;
 
 const gridSize = 500;
 const scale = document.getElementById('scale');
@@ -12,39 +13,32 @@ const pixelContainer = document.querySelector('.pixel-container');
 const colorSwatch = document.querySelector('.color-swatch');
 const colorBtn = document.querySelector('.color-button');
 const randomBtn = document.querySelector('.random-button');
+const eraserBtn = document.querySelector('.eraser-button')
 const clearBtn = document.querySelector('.clear-button');
 
-
+colorBtn.onclick = () => changeMode('color');
+randomBtn.onclick = () => changeMode('random');
+eraserBtn.onclick = () => changeMode('eraser');
 clearBtn.onclick = () => changeSize(currentSize);
 colorSwatch.onchange = (e) => updateColorSwatch(e.target.value);
 scale.onmousemove = (e) => updateSize(e.target.value);
 scale.onchange = (e) => changeSize(e.target.value);
 
 let mouseDown = false;
-
 pixelContainer.addEventListener('mousedown', () => {
   mouseDown = true;
 })
-
 pixelContainer.addEventListener('mouseup', () => {
   mouseDown = false;
 })
 
-// clearBtn.addEventListener('click', () => clearPixelContainer(currentSize));
-// document.body.onmousedown = () => (mouseDown = true);
-// document.body.onmouseup = () => (mouseDown = false);
-
-// document.addEventListener('mouseover', (e) => {
-//   if (mouseDown) {
-//     if (e.target.className === 'pixel') {
-//       e.target.style.backgroundColor = currentColor;
-//     }
-//   }
-// })
-
-
 function updateColorSwatch(newColor) {
   currentColor = newColor;
+}
+
+function updateMode(newMode) {
+  changeMode(newMode);
+  currentMode = newMode;
 }
 
 function updateSize(size) {
@@ -74,23 +68,48 @@ function setPixelContainer(size) {
   }
 }
 
+function randomColor() {
+  const randR = Math.floor(Math.random() * 256);
+  const randG = Math.floor(Math.random() * 256);
+  const randB = Math.floor(Math.random() * 256);
+  return `rgb(${randR}, ${randG}, ${randB})`
+}
+
 function changeColor(e) {
   if (e.type === 'mouseover' && !mouseDown) return
-  e.target.style.backgroundColor = currentColor;
+  if (currentMode === 'random') {
+    e.target.style.backgroundColor = randomColor()
+  } else if (currentMode === 'color') {
+    e.target.style.backgroundColor = currentColor
+  } else if (currentMode === 'eraser') {
+    e.target.style.backgroundColor = 'var(--white)'
+  }
+}
+
+function changeMode(newMode) {
+  const depressedBtns = document.querySelectorAll('.depressed')
+  depressedBtns.forEach((button) => {
+    button.classList.remove('depressed')
+  });
+
+  currentMode = newMode
+
+  switch(newMode) {
+    case('random'):
+      randomBtn.classList.add('depressed');
+      break;
+    case('color'):
+      colorBtn.classList.add('depressed');
+      break;
+    case('eraser'):
+      eraserBtn.classList.add('depressed');
+      break;
+  }
 }
 
 window.onload = () => {
   setPixelContainer(DEFAULT_SIZE)
   scale.value = DEFAULT_SIZE;
   colorSwatch.value = DEFAULT_COLOR;
-}
-
-
-
-// HELPER FUNCTIONS
-
-function width() {
-  let style = getComputedStyle(pixelContainer);
-  let pixels = style.width;
-  return pixels.slice(0, -2);
+  colorBtn.classList.add('depressed');
 }
